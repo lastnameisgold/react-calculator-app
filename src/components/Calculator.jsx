064bf5329c62aa14d14a54f839a2b989b85ce257
Fernando <me@fdorado.com>
2023-02-17 23:24:55 -0500

    Fixed percentage and added or condition for equals. Now you can press enter key or equal key to calculate.

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,41 +1,83 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 
-export default function Calculator () {
+export default function Calculator() {
+  const [display, setDisplay] = useState(0);
+  const [currentCalc, setCurrentCalc] = useState('');
 
-    const [ display, setDisplay ] = useState(0);
-    const [currentCalc, setCurrentCalc] = useState('');
-    
-    const handleClick = (event) => {
-        event.preventDefault();
-        const newValue = currentCalc + event.target.value
-        console.log(newValue)
-        setCurrentCalc(newValue)
-        setDisplay(newValue)
+  const handleClick = (event) => {
+    event.preventDefault();
+    const newValue = currentCalc + event.target.value;
+    setCurrentCalc(newValue);
+    setDisplay(newValue);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setDisplay(eval(currentCalc));
+    let total = eval(currentCalc);
+    setCurrentCalc(total);
+  }
+
+  const resetCalc = (event) => {
+    event.preventDefault();
+    setDisplay(0);
+    setCurrentCalc('');
+  }
+
+  const backSpace = (event) => {
+    event.preventDefault();
+    const newValue = currentCalc.slice(0, -1);
+    setCurrentCalc(newValue);
+    if (newValue === '') {
+      setDisplay('0');
+    } else {
+      setDisplay(newValue);
     }
+  }
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setDisplay(eval(currentCalc));
-        let total = eval(currentCalc)
-        setCurrentCalc(total)
-    }
+  const handlePercent = () => {
+    const newValue = currentCalc / 100;
+    setCurrentCalc(newValue);
+    setDisplay(newValue);
+  }
 
-    const resetCalc = (event) => {
-        event.preventDefault();
-        setDisplay(0);
-        setCurrentCalc('');
-    }
-
-    const backSpace = (event) => {
-        event.preventDefault();
-        const newValue = currentCalc.slice(0, -1);
-        setCurrentCalc(newValue);
-        if(newValue == '') {
-            setDisplay('0')
-        } else {
-            setDisplay(newValue);
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+        // Backspace key
+        if (event.keyCode === 8) {
+            event.preventDefault();
+            backSpace(event);
+        } 
+        // Escape key
+        else if (event.keyCode === 27) {
+            event.preventDefault();
+            resetCalc(event);
         }
+        // Percent key
+        else if (/^[%]$/.test(event.key)) {
+            event.preventDefault();
+            handlePercent(event);
+        } 
+        // Enter Key
+        else if (event.keyCode === 13 || /^[=]$/.test(event.key)) {
+            event.preventDefault();
+            handleSubmit(event);
+        }
+        // Any number from 0-9 and operators
+        else if (/^[0-9\+\-\*\/\(\)\.]$/.test(event.key)) {
+            event.preventDefault();
+            const newValue = currentCalc + event.key;
+            setCurrentCalc(newValue);
+            setDisplay(newValue);
+        } 
+      }
+
+    window.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
     }
+  }, [currentCalc])
 
     return (
         <main id="calculator-container">
@@ -47,7 +89,7 @@ export default function Calculator () {
         <section id="buttons-container">
             <button className="calc-button tertiary-btn" onClick={resetCalc}>AC</button>
             <button className="calc-button tertiary-btn" onClick={backSpace}><span className="material-symbols-rounded size-48">backspace</span></button>
-            <button className="calc-button tertiary-btn" value="%" onClick={handleClick}>{'\u0025'}</button>
+            <button className="calc-button tertiary-btn" value="%" onClick={handlePercent}>{'\u0025'}</button>
             <button className="calc-button primary-btn" value="/" onClick={handleClick}>{'\u00f7'}</button>
             <button className="calc-button primary-container-btn" value="7" onClick={handleClick}>7</button>
             <button className="calc-button primary-container-btn" value="8" onClick={handleClick}>8</button>
